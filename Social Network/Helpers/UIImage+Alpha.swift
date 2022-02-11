d29be35f4abe5bd5c95a9ccbd3a4330970b6e3f5
Arthur Raff <arthur.raff@icloud.com>
2022-02-11 19:48:35 +0300

    UIImage+Alpha extension refactored

--- Social Network/Helpers/UIImage+Alpha.swift
+++ Social Network/Helpers/UIImage+Alpha.swift
@@ -3,10 +3,13 @@ import UIKit
 extension UIImage {
     func alpha(_ value: CGFloat) -> UIImage {
         UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        
         draw(at: CGPoint.zero, blendMode: .normal, alpha: value)
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        guard let newImage = UIGraphicsGetImageFromCurrentImageContext() else { return UIImage() }
+        
         UIGraphicsEndImageContext()
         
-        return newImage!
+        return newImage
     }
 }
