098afd4331a544bc3101a85240b6fe5be37ffd41
Arthur Raff <arthur.raff@icloud.com>
2022-04-25 22:16:29 +0300

    SocialNetworkFileManager edited and refactored

--- Social Network/SocialNetworkFileManager/SocialNetworkFileManager.swift
+++ Social Network/SocialNetworkFileManager/SocialNetworkFileManager.swift
@@ -7,40 +7,70 @@
 
 import Foundation
 
-class SocialNetworkFileManager {
-    private let manager = FileManager.default
+enum FileManagerDirectory {
+    case documentDirectory
+    case userDirectory
     
-    func getDefault(directory: FileManager.SearchPathDirectory,
-                    domain: FileManager.SearchPathDomainMask ) -> URL? {
-        return try? manager.url(for: directory,
-                                in: domain,
-                                appropriateFor: nil,
-                                create: false)
+    var url: URL? {
+        switch self {
+        case .documentDirectory:
+            return try? FileManager.default.url(for: .documentDirectory,
+                                                in: .userDomainMask,
+                                                appropriateFor: nil,
+                                                create: false)
+        case .userDirectory:
+            return try? FileManager.default.url(for: .userDirectory,
+                                                in: .userDomainMask,
+                                                appropriateFor: nil,
+                                                create: false)
+        }
+    }
+}
+
+final class SocialNetworkFileManager {    
+    func getFilesFrom(directory: FileManagerDirectory,
+                      completion: ([URL]?) -> Void) {
+        guard let directoryUrl = directory.url else { return }
+        
+        let files = try? FileManager.default.contentsOfDirectory(at: directoryUrl,
+                                                                 includingPropertiesForKeys: nil,
+                                                                 options: [.skipsHiddenFiles])
+        
+        completion(files)
     }
     
-    func getFileAttributesAt(path: String) -> [FileAttributeKey: Any]? {
-        return try? manager.attributesOfItem(atPath: path)
-    }
-    
-    func createDirectoryAt(path: String) {
-        do {
-            try manager.createDirectory(atPath: path,
-                                        withIntermediateDirectories: true)
-        } catch {
-            print(error)
+    func getFileAttributesFrom(directory: FileManagerDirectory,
+                               atName name: String,
+                               completion: ([FileAttributeKey: Any]?) -> Void ) {
+        guard let directoryUrl = directory.url else { return }
+        
+        let fileUrl = directoryUrl.appendingPathComponent(name)
+        let fileExist = FileManager.default.fileExists(atPath: fileUrl.path)
+        
+        if fileExist == true {
+            let attributes = try? FileManager.default.attributesOfItem(atPath: fileUrl.path)
+            
+            completion(attributes)
         }
     }
     
-    func create(file: Data, path: String) {
-        manager.createFile(atPath: path,
-                           contents: file)
+    func create(file: Data,
+                withName name: String,
+                atDirectory directory: FileManagerDirectory) {
+        guard let directoryUrl = directory.url else { return }
+        
+        let fileUrl = directoryUrl.appendingPathComponent(name)
+
+        FileManager.default.createFile(atPath: fileUrl.path,
+                                       contents: file)        
     }
     
-    func removeFileOrDirectoryAt(path: String) {
-        do {
-            try manager.removeItem(atPath: path)
-        } catch (let error) {
-            print(error)
-        }
+    func removeFileAt(directory: FileManagerDirectory,
+                      withName name: String) {
+        guard let directoryUrl = directory.url else { return }
+        
+        let fileUrl = directoryUrl.appendingPathComponent(name)
+        
+        try? FileManager.default.removeItem(atPath: fileUrl.path)
     }
 }
