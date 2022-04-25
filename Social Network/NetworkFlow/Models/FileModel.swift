27a5eeda34c972ca06aa60ad431d98f8055579c9
Arthur Raff <arthur.raff@icloud.com>
2022-04-25 22:15:01 +0300

    FileModel added

---
+++ Social Network/NetworkFlow/Models/FileModel.swift
@@ -0,0 +1,16 @@
+//
+//  FileModel.swift
+//  Social_Network
+//
+//  Created by Arthur Raff on 24.04.2022.
+//
+
+import Foundation
+
+struct File: Codable {
+    let url: String?
+    let name: String?
+    let size: String?
+    let type: String?
+    let creationDate: String?
+}
