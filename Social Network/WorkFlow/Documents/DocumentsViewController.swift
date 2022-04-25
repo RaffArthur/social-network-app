4ae0c024e60d7b1a0c54cf7820ada0be3747117f
Arthur Raff <arthur.raff@icloud.com>
2022-04-25 22:16:41 +0300

    DocumentsViewController edited and refactored

--- Social Network/WorkFlow/Documents/DocumentsViewController.swift
+++ Social Network/WorkFlow/Documents/DocumentsViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 final class DocumentsViewController: UIViewController {
-    private lazy var addPhotoButton: UIBarButtonItem = {
+    private var files: [File] = []
+    
+    private let fileManager = SocialNetworkFileManager()
+    
+    private lazy var imagePickerController: UIImagePickerController = {
+        let ipc = UIImagePickerController()
+        ipc.delegate = self
+        
+        return ipc
+    }()
+
+    private lazy var addFileButton: UIBarButtonItem = {
         let button = UIBarButtonItem()
         button.image = UIImage(systemName: "doc.badge.plus")
         
@@ -28,8 +39,9 @@ final class DocumentsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupScreen()
+        getFiles()
         
+        setupScreen()
         setupActions()
     }
 }
@@ -50,21 +62,72 @@ private extension DocumentsViewController {
     
     func setupContent() {
         view.backgroundColor = .white
+        tableView.backgroundColor = .white
         
-        title = "Документы"
+        title = "Файлы"
 
-        navigationItem.rightBarButtonItem = addPhotoButton
+        navigationItem.rightBarButtonItem = addFileButton
+    }
+}
+
+private extension DocumentsViewController {
+    func getFiles() {
+        fileManager.getFilesFrom(directory: .documentDirectory) { files in
+            let filesData = files?.compactMap {
+                File(url: $0.absoluteString,
+                     name: $0.lastPathComponent,
+                     size: getFileAttributeBy(name: $0.lastPathComponent).size,
+                     type: getFileAttributeBy(name: $0.lastPathComponent).type,
+                     creationDate: getFileAttributeBy(name: $0.lastPathComponent).creationDate)
+            }
+            
+            guard let data = filesData else { return }
+            
+            self.files = data
+            
+            tableView.reloadData()
+        }
+    }
+    
+    func getFileAttributeBy(name: String) -> (size: String,
+                                              creationDate: String,
+                                              type: String) {
+        var convertedAttributes = (size: String(),
+                                   creationDate: String(),
+                                   type: String())
+        
+        fileManager.getFileAttributesFrom(directory: .documentDirectory,
+                                          atName: name) { attributes in
+            
+            guard let creationDate = attributes?[.creationDate] as? Date,
+                  let size = attributes?[.size] as? Double,
+                  let type = attributes?[.type] as? String
+            else {
+                return
+            }
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .long
+            dateFormatter.timeZone = TimeZone(identifier: "RU")
+            dateFormatter.locale = Locale(identifier: "RU")
+            
+            convertedAttributes.creationDate = dateFormatter.string(from: creationDate)
+            convertedAttributes.size = "\( round(size / 1024)) Кб"
+            convertedAttributes.type = "\(type)"
+        }
+        
+        return convertedAttributes
     }
 }
 
 private extension DocumentsViewController {
     func setupActions() {
-        addPhotoButton.action = #selector(addFileToDocuments)
-        addPhotoButton.target = self
+        addFileButton.action = #selector(addFileToDocuments)
+        addFileButton.target = self
     }
     
     @objc func addFileToDocuments() {
-        let imagePickerController = UIImagePickerController()
+        imagePickerController.modalPresentationStyle = .automatic
         
         present(imagePickerController, animated: true)
     }
@@ -79,6 +142,7 @@ extension DocumentsViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView,
                    heightForRowAt indexPath: IndexPath) -> CGFloat {
+
         return UITableView.automaticDimension
     }
 }
@@ -86,7 +150,7 @@ extension DocumentsViewController: UITableViewDelegate {
 extension DocumentsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return files.count
     }
     
     func tableView(_ tableView: UITableView,
@@ -95,6 +159,48 @@ extension DocumentsViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: identifier ,
                                                  for: indexPath) as? DocumentsTableViewCell
         
+        cell?.configure(file: files[indexPath.row])
+        
         return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let trashAction = UIContextualAction(style: .destructive,
+                                             title: "Удалить") { action, view, success in
+            
+            guard let name = self.files[indexPath.row].name else { return }
+            
+            self.fileManager.removeFileAt(directory: .documentDirectory,
+                                          withName: name)
+            
+            self.files.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+            success(true)
+        }
+        
+        return UISwipeActionsConfiguration(actions: [trashAction])
+    }
+}
+
+extension DocumentsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = (info[.originalImage] as? UIImage)?.jpegData(compressionQuality: 0.2),
+              let name = (info[.imageURL] as? URL)?.lastPathComponent
+        else {
+            return
+        }
+        
+        fileManager.create(file: image,
+                           withName: name,
+                           atDirectory: .documentDirectory)
+        
+        picker.dismiss(animated: true)
+        
+        getFiles()
     }
 }
