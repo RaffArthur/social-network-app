dd0842e4cdc4e4682b1355678e3304d2b9c8dae5
Arthur Raff <arthur.raff@icloud.com>
2022-05-29 16:41:30 +0300

    CoreDataManager refactored, local Storages created

--- Social Network/CoreData/CoreDataManager.swift
+++ Social Network/CoreData/CoreDataManager.swift
@@ -6,7 +6,64 @@
 //
 
 import Foundation
+import CoreData
 
-protocol CoreDataManager: AnyObject {
-    func load(completion: (() -> Void)?)
+final class CoreDataManager {
+    static let shared = CoreDataManager(modelName: "SocialNetworkCoreData")
+    
+    private let persistentContainer: NSPersistentContainer
+    
+    init(modelName: String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load(completion: (() -> Void)?) {
+        persistentContainer.loadPersistentStores { description, error in
+            guard error == nil else { return }
+            
+            completion?()
+        }
+    }
+    
+    private func save() {
+        if persistentContainer.viewContext.hasChanges {
+            do {
+                try persistentContainer.viewContext.save()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension CoreDataManager {
+    func saveToFavourite(post: Post) {
+        let favouritePost = FavouritePost(context: persistentContainer.viewContext)
+        favouritePost.title = post.title
+        favouritePost.body = post.body
+        
+        save()        
+    }
+    
+    func fetchFavouritePosts() -> [FavouritePost] {
+        let request = FavouritePost.fetchRequest()
+        
+        let favouritePosts = try? persistentContainer.viewContext.fetch(request)
+        
+        return favouritePosts ?? []
+    }
+    
+    func removePostFrom(favouritePosts: FavouritePost) {
+        persistentContainer.viewContext.delete(favouritePosts)
+        
+        save()
+    }
+    
+    func deletAll() {
+        fetchFavouritePosts().forEach {
+            persistentContainer.viewContext.delete($0)
+        }
+        
+        save()
+    }
 }
