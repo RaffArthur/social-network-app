716047e35ac28385653ac443210e78c0b0603ec8
Arthur Raff <arthur.raff@icloud.com>
2022-10-01 19:32:15 +0300

    AuthenticationReviewerTests refactored

--- Social_NetworkTests/AuthenticationReviewerTests.swift
+++ Social_NetworkTests/AuthenticationReviewerTests.swift
@@ -11,7 +11,7 @@ import Firebase
 @testable import Social_Network
 
 final class AuthenticationReviewerTests: XCTestCase {
-    var mockAuthReviewer: MockAuthenticationReviewer?
+    var mockAuthReviewer: MockAuthenticationReviewer!
     
     override func setUp() {
         super.setUp()
@@ -20,73 +20,76 @@ final class AuthenticationReviewerTests: XCTestCase {
     }
     
     func test_singin_response() {
-        let expectation = expectation(description: "Some exp")
-
-        let credentials = UserCredentials(email: "aa@aa.a",
+        let credentials = UserCredentials(email: "aa@aa.aa",
                                           password: "aaaaaa",
                                           repeatPassword: nil,
                                           loggedIn: true)
-                        
-        mockAuthReviewer?.signInWith(credentials: credentials) { result in
+                
+        let expectation = expectation(description: "Singin expectation")
+                
+        mockAuthReviewer.signInWith(credentials: credentials) { result in
             switch result {
-            case .success:
-                XCTAssertTrue(true)
+            case .success(let response):
+                XCTAssertNotNil(response)
             case .failure(let error):
-                XCTFail("\(error.title). Описание ошибки: \(error.message)")
+                XCTFail("Ошибка: \(error.title). Описание ошибки: \(error.message)")
             }
             
             expectation.fulfill()
         }
         
-        XCTAssertTrue(mockAuthReviewer?.signInWasCalled == true)
+        XCTAssertTrue(mockAuthReviewer.signInWasCalled)
+        XCTAssertFalse(mockAuthReviewer.shouldReturnError)
         
         waitForExpectations(timeout: 2.0)
     }
     
     func test_signout_response() {
-        let expectation = expectation(description: "Some exp")
-
-        let credentials = UserCredentials(email: "aa@aa.a",
+        let credentials = UserCredentials(email: "aa@aa.aa",
                                           password: "aaaaaa",
                                           repeatPassword: nil,
                                           loggedIn: false)
         
+        let expectation = expectation(description: "Some exp")
+        
         mockAuthReviewer?.signOutWith(credentials: credentials) { result in
             switch result {
-            case .success:
-                XCTAssertTrue(true)
+            case .success(let response):
+                XCTAssertTrue(response as! Bool)
             case .failure(let error):
-                XCTFail("\(error.title). Описание ошибки: \(error.message)")
+                XCTFail("Ошибка: \(error.title). Описание ошибки: \(error.message)")
             }
             
             expectation.fulfill()
         }
         
-        XCTAssertTrue(mockAuthReviewer?.signOutWasCalled == true)
-
+        XCTAssertTrue(mockAuthReviewer.signOutWasCalled)
+        XCTAssertFalse(mockAuthReviewer.shouldReturnError)
+        
         waitForExpectations(timeout: 2.0)
     }
     
     func test_registration_response() {
         let expectation = expectation(description: "Some exp")
 
-        let credentials = UserCredentials(email: "aa@aa.aa",
+        let credentials = UserCredentials(email: "aa@aa.aas",
                                           password: "aaaaaa",
                                           repeatPassword: "aaaaaa",
                                           loggedIn: true)
                 
         mockAuthReviewer?.registrationWith(credentials: credentials) { result in
             switch result {
-            case .success:
-                XCTAssertTrue(true)
+            case .success(let response):
+                XCTAssertNotNil(response)
             case .failure(let error):
-                XCTFail("\(error.title). Описание ошибки: \(error.message)")
+                XCTFail("Ошибка: \(error.title). Описание ошибки: \(error.message)")
             }
             
             expectation.fulfill()
         }
         
-        XCTAssertTrue(mockAuthReviewer?.registrationWasCalled == true)
+        XCTAssertTrue(mockAuthReviewer.registrationWasCalled)
+        XCTAssertFalse(mockAuthReviewer.shouldReturnError)
 
         waitForExpectations(timeout: 2.0)
     }
@@ -96,38 +99,102 @@ final class MockAuthenticationReviewer {
     var signInWasCalled = false
     var signOutWasCalled = false
     var registrationWasCalled = false
-    var shouldReturnError = false
+    var shouldReturnError: Bool = Bool()
+    
+    let errorCodeConverter: AuthErrorCodeConverter = AuthErrorCodeConverterImpl()
+    let realmDataProvider: RealmUserCredentialsDataProvider = RealmUserCredentialsDataProviderImpl()
+
 }
 
 extension MockAuthenticationReviewer: AuthentificationReviewer {
     func signOutWith(credentials: UserCredentials,
-                     completion: (Result<Any, UserAuthError>) -> Void) {
+                     completion: UserAuthResult) {
         signOutWasCalled = true
         
-        if shouldReturnError {
-            completion(.failure(.internalError))
-        } else {
+        do {
+            shouldReturnError = false
+            
             completion(.success(true))
+            
+            realmDataProvider.updateUser(credentials: credentials)
+            
+            try Auth.auth().signOut()
+        } catch {
+            shouldReturnError = true
+            
+            completion(.failure(.keyсhainError))
         }
     }
     
-    func signInWith(credentials: UserCredentials, completion: @escaping UserAuthResult) {
+    func signInWith(credentials: UserCredentials,
+                    completion: @escaping UserAuthResult) {
         signInWasCalled = true
         
-        if shouldReturnError {
-            completion(.failure(.internalError))
-        } else {
-            completion(.success(true))
+        guard let userEmail = credentials.email,
+              let userPassword = credentials.password
+        else {
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: userEmail,
+                           password: userPassword) { authResult, error in
+            guard error == nil else {
+                self.shouldReturnError = true
+                
+                if let nsError = error as NSError?,
+                   let code = AuthErrorCode.Code(rawValue: nsError.code){
+                    
+                    guard let bussinesLogicError = self.errorCodeConverter.convertAuthError(code: code) else { return }
+                    completion(.failure(bussinesLogicError))
+                } else {
+                    completion(.failure(.unknownError))
+                }
+                                
+                return
+            }
+            
+            guard let user = authResult?.user else { return }
+            
+            self.shouldReturnError = false
+            
+            completion(.success(user))
         }
     }
     
-    func registrationWith(credentials: UserCredentials, completion: @escaping UserAuthResult) {
+    func registrationWith(credentials: UserCredentials,
+                          completion: @escaping UserAuthResult) {
         registrationWasCalled = true
         
-        if shouldReturnError {
-            completion(.failure(.internalError))
-        } else {
-            completion(.success(true))
+        guard let userEmail = credentials.email,
+              let userPassword = credentials.password
+        else {
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: userEmail,
+                               password: userPassword) { authResult, error in
+            
+            guard error == nil else {
+                self.shouldReturnError = true
+                
+                if let nsError = error as NSError?,
+                   let code = AuthErrorCode.Code(rawValue: nsError.code) {
+                    
+                    guard let bussinesLogicError = self.errorCodeConverter.convertAuthError(code: code) else { return }
+                    
+                    completion(.failure(bussinesLogicError))
+                } else {
+                    completion(.failure(.unknownError))
+                }
+                
+                return
+            }
+            
+            guard let user = authResult?.user else { return }
+            
+            self.shouldReturnError = false
+            
+            completion(.success(user))
         }
     }
 }
